83cee6d153e79231cec97b01678ba93be529d6e3
513609 <513609jb@eur.nl>
2025-12-26 21:23:42 +0100

    fix: add instrumentation.ts to override failing auto-gen

---
+++ saleor-payload/src/instrumentation.ts
@@ -0,0 +1,4 @@
+export async function register() {
+  // Define register to override Vercel's auto-generated one that fails to find Sentry configs
+  // We can leave this empty or add custom instrumentation here later.
+}
