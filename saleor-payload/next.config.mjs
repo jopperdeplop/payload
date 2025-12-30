0607e72e5724d62c6485731b335d8ebf42316a4c
513609 <513609jb@eur.nl>
2025-12-30 10:12:13 +0100

    Update - test later

--- saleor-payload/next.config.mjs
+++ saleor-payload/next.config.mjs
@@ -1,8 +1,17 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  serverExternalPackages: ['pino-pretty'],
+  sassOptions: {
+    includePaths: [path.join(dirname, 'node_modules')],
+  },
 }
 
 export default withPayload(nextConfig)
